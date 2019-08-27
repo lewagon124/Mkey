d6aca6c5197e947ae0d62e02a31e2cf1a8e025ff
Seong Park <eslteach124@gmail.com>
2019-08-27 10:42:11 -0400

    MigrationWorks

--- db/migrate/20190826183654_create_relationships.rb
+++ db/migrate/20190826183654_create_relationships.rb
@@ -2,7 +2,7 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
   def change
     create_table :relationships do |t|
       t.references :user, foreign_key: true
-      t.references :friend, foreign_key: true
+      t.integer :friend_user_id
       t.timestamps
     end
   end
