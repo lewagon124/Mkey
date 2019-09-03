1d5eb52aad3355586ea81343cbc67180faa8abb0
Huiran Chen <huiran.chen@gmail.com>
2019-09-03 11:16:15 -0700

    videospeed

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,15 +1,12 @@
+var navbarBG = document.querySelector(".navbar-lewagon");
+navbarBG.style.position = "fixed";
+navbarBG.style.background = "rgba(251, 205, 119,0)";
+
 window.addEventListener("scroll", function() {
   let offset = window.pageYOffset;
-
   // console.log(offset);
   var navbarBG = document.querySelector(".navbar-lewagon");
   // console.log(navbarBG);
   navbarBG.style.background= `rgba(251, 205, 119,${offset/window.innerHeight})`;
 
-  //   var parallax = document.getElementsByClassName("mission");
-  //   parallax[0].style.backgroundPositionY = offset * 0.1 - 200 + "px";
-  //
-  // var parallaxCar = document.getElementsByClassName("mission-car");
-  // parallaxCar[0].style.marginLeft= (window.pageYOffset/1.6)-500 + 'px';
-  // console.log((window.pageYOffset/ 4) + 'px');
   })
